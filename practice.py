752ee9772969df9a5e5f394f1abfd3fae82cbb49
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2020-06-07 00:35:15 +0530

    Add new script to create a table in latex

--- practice.py
+++ practice.py
@@ -1,1 +1,8 @@
 print('Hello world!')
+def createTable(num_cols):
+    with open('output.txt','a') as out:
+        out.write('\begin{table}[!h]')
+        out.write('\begin/{tabular/}/{' +'c '*num_cols+ '/}')
+
+
+createTable(3)
